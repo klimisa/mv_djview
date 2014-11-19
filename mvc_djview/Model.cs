787cac17daea20950627024fd926f5834303f9e9
Antonios Klimis <antonios.klimis@gmail.com>
2014-11-19 22:56:18 +0200

    Added Controller & Model class

---
+++ mvc_djview/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mvc_djview
+{
+    public class Model
+    {
+        public int BPM { get; set; }
+
+        public Model()
+        {
+            BPM = 120;
+        }
+
+    }
+}
